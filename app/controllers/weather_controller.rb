5d937fc46ccc8f1ba50a0083998a58938e0e67b0
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-20 23:37:58 +0900

    add_comment

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -23,7 +23,7 @@ class WeatherController < ApplicationController
 
     events = client.parse_events_from(body)
     events.each { |event|
-      
+      # open_weatherのAPIを呼び出す
       if event.message['text'].include?("天気")
         city = event.message['text'].delete(" 天気")
         response = open_weather = "http://api.openweathermap.org/data/2.5/weather?q=#{city},jp&units=metric&lang=ja&APPID=2a8d665689d5a8d78c32f0ab119e6948"
@@ -32,10 +32,12 @@ class WeatherController < ApplicationController
         tutorial = "都市の名前の後ろにスペースを開けずに天気と入力してください。" 
         response = "#{tutorial}"
       elsif
+        # ぐるなびAPIを呼び出す
         event.message['text'].include?("ぐるなび")
         store = event.message['text'].delete("ぐるなび")
         response = store = "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317&address=#{store}"
       else
+        # おうむ返し
         event.message['text']
         same = event.message['text']
         response = "#{same}"
