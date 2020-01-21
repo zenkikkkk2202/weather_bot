83675d4ef5b454fa0669387ff6129569c6efae78
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-21 12:48:11 +0900

    search_navi

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/callback' => 'weather#callback'
 end
+
