0a3309ef689b306bfc0e9700b8c9c5434fd47fb3
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-21 12:34:34 +0900

    add_id

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -77,12 +77,12 @@ class WeatherController < ApplicationController
     events.each { |event|
       if event.message['text'] != nil
         place = event.message['text'] #ここでLINEで送った文章を取得
-        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid='&'format=json'&'address=#{place}`#ここでぐるなびAPIを叩く
+        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=161a20d6368441dd8e7d27c1aa717317'&'format=json'&'address=#{place}`#ここでぐるなびAPIを叩く
       else
         latitude = event.message['latitude']
         longitude = event.message['longitude']
 
-        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid='&'format=json'&'latitude=#{latitude}'&'longitude=#{longitude}`#ここでぐるなびAPIを叩く
+        result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=161a20d6368441dd8e7d27c1aa717317'&'format=json'&'latitude=#{latitude}'&'longitude=#{longitude}`#ここでぐるなびAPIを叩く
       end
 
       hash_result = JSON.parse result #レスポンスが文字列なのでhashにパースする
