a871d0fbaf3bc218553629f9c40a0e0e9c427f74
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-23 01:45:52 +0900

    chnage

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -49,7 +49,7 @@ class WeatherController < ApplicationController
         area = event.message['text'].delete("ぐるなび")
         # response = `curl -X GET "https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317&address=#{area}"`
         eurl = URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317&address=#{area}")
-        uri = URI.parse("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317&address=#{area}")
+        uri = URI.parse("#{eurl}")
         aaa = Net::HTTP.get_response(uri)
         response = `curl -X GET #{aaa}`
       else
