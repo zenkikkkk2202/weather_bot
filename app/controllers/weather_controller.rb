420a52dc054fd6bddfc138e3219f693386e9c072
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-22 17:18:59 +0900

    change

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -35,10 +35,10 @@ class WeatherController < ApplicationController
         city = event.message['text'].delete(" 天気")
         open_weather = `curl -X GET "http://api.openweathermap.org/data/2.5/weather?q=#{city},jp&units=metric&lang=ja&APPID=2a8d665689d5a8d78c32f0ab119e6948"`
         hash_result = JSON.parse open_weather#レスポンスが文字列なのでhashにパースする
-        weather = hash_result.fetch("weather")
+        tenki = hash_result.fetch("weather")
         main = hash_result.fetch("main")
         # result = "天気 #{descriptioin.fetch("description")} 最高気温 #{main.fetch("temp_max")} 最低気温 #{main.fetch("temp_min")}"
-        response = "平均気温 #{main.fetch("temp")} \n 最高気温 #{main.fetch("temp_max")} \n 最低気温 #{main.fetch("temp_min")}"
+        response = "天気 #{tenki.fetch("main")} \n 詳細 #{tenki.fetch("description")} \n 平均気温 #{main.fetch("temp")} \n 最高気温 #{main.fetch("temp_max")} \n 最低気温 #{main.fetch("temp_min")}"
       elsif
         # ぐるなびAPIを呼び出す
         event.message['text'].include?("ぐるなび")
