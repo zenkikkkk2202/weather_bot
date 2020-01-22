cc904fad2f49b3516764dda418566ed87f293500
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-22 14:22:00 +0900

    check

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -38,7 +38,7 @@ class WeatherController < ApplicationController
         event.message['text'].include?("ぐるなび")
         area = event.message['text'].delete("ぐるなび")
         area.force_encoding('UTF-8')
-        response = `curl -X GET https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317'&'format=json'&'address=#{area}`
+        response = `curl -X GET https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317'&'id=#{area}`
       else
         # おうむ返し
         event.message['text']
