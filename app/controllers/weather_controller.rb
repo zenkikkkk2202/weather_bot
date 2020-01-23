49756156dadb53cade14805f099aaa478f789ce0
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-23 17:46:19 +0900

    change

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -62,7 +62,7 @@ class WeatherController < ApplicationController
         area = event.message['text'].delete("ぐるなび")
         gkey = ENV["GURU_KEY"]
         eurl = URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317&address=#{area}")
-        response = `curl -X GET "#{eurl}"`
+        response = `curl -s -X GET "#{eurl}"`
         # result = open("#{eurl}",&:read)
         # response = "#{result}"
       else
