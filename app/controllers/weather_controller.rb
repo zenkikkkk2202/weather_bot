19e30353beb075e4df5857f5d67c1143e052eff7
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-23 19:18:02 +0900

    change

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -29,9 +29,8 @@ class WeatherController < ApplicationController
       # open_weatherのAPIを呼び出す
       if 
         event.message['text'] == "チュートリアル"
-        tutorial = "都市の名前の後ろにスペースを開けずに天気と入力してください。" 
+        tutorial = "地名の後ろにスペースを開けずに天気と入力すると、天気予報が返ってきます \nニュースと入力するとトップニュースが返ります \n地名の後ろにスペースを開けずにぐるなびと入力すると、お店の情報が返ります \nそれ以外はおうむ返しです" 
         response = "#{tutorial}"  
-         
       elsif 
         event.message['text'].include?("天気")
         city = event.message['text'].delete(" 天気")
@@ -56,8 +55,7 @@ class WeatherController < ApplicationController
         info = nresult.fetch("articles")[0]
         title = info.fetch("title")
         url = info.fetch("url")
-        response = "タイトル #{title} \n URL #{url}"
-        # `curl -X GET "http://newsapi.org/v2/top-headlines?country=jp&apiKey=#{nkey}"`
+        response = "タイトル #{title} \nURL #{url}"
       elsif
         # ぐるなびAPIを呼び出す
         event.message['text'].include?("ぐるなび")
@@ -94,62 +92,3 @@ class WeatherController < ApplicationController
   end
   
 end
-
-  # def callback
-  #   body = request.body.read
-
-  #   signature = request.env['HTTP_X_LINE_SIGNATURE']
-  #   unless client.validate_signature(body, signature)
-  #     error 400 do 'Bad Request' end
-  #   end
-
-  #   events = client.parse_events_from(body)
-
-  #   #ここでlineに送られたイベントを検出している
-  #   # messageのtext: に指定すると、返信する文字を決定することができる
-  #   #event.message['text']で送られたメッセージを取得することができる
-  #   events.each { |event|
-  #     if event.message['text'] != nil
-  #       place = event.message['text'] #ここでLINEで送った文章を取得
-  #       # result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=161a20d6368441dd8e7d27c1aa717317'&'format=json'&'address=#{place}`#ここでぐるなびAPIを叩く
-  #       result = `curl -X GET https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=161a20d6368441dd8e7d27c1aa717317'&'format=json'&'address=#{place}`#ここでぐるなびAPIを叩く
-  #     else
-  #       latitude = event.message['latitude']
-  #       longitude = event.message['longitude']
-  #       result = `curl -X GET http://api.gnavi.co.jp/RestSearchAPI/20150630/?keyid=161a20d6368441dd8e7d27c1aa717317'&'format=json'&'latitude=#{latitude}'&'longitude=#{longitude}`#ここでぐるなびAPIを叩く
-  #     end
-
-  #     hash_result = JSON.parse result #レスポンスが文字列なのでhashにパースする
-  #     shops = hash_result["rest"] #ここでお店情報が入った配列となる
-      #任意のものを一個選ぶ
-
-  #     #店の情報
-  #     url = shop["url_mobile"] #サイトのURLを送る
-  #     shop_name = shop["name"] #店の名前
-  #     category = shop["category"] #カテゴリー
-  #     open_time = shop["opentime"] #空いている時間
-  #     holiday = shop["holiday"] #定休日
-
-  #     if open_time.class != String #空いている時間と定休日の二つは空白の時にHashで返ってくるので、文字列に直そうとするとエラーになる。そのため、クラスによる場合分け。
-  #       open_time = ""
-  #     end
-
-  #     if holiday.class != String
-  #       holiday = ""
-  #     end
-
-  #     response = "【店名】" + shop_name + "\n" + "【カテゴリー】" + category + "\n" + "【営業時間と定休日】" + open_time + "\n" + holiday + "\n" + url
-  #      case event #case文　caseの値がwhenと一致する時にwhenの中の文章が実行される(switch文みたいなもの)
-  #     when Line::Bot::Event::Message
-  #       case event.type
-  #       when Line::Bot::Event::MessageType::Text,Line::Bot::Event::MessageType::Location
-  #         message = {
-  #           type: 'text',
-  #           text: response
-  #         }
-  #         client.reply_message(event['replyToken'], message)
-  #       end
-
-  #     end
-  #   } 
-
