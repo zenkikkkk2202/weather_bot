e273177fd0281283843037fedacc2f9f6a11363f
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-23 14:48:37 +0900

    change

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -40,7 +40,7 @@ class WeatherController < ApplicationController
         tenki = hash_result.fetch("weather")[0]
         main = hash_result.fetch("main")
         if status == 200
-          response = " 天気 #{tenki.fetch("main")} \n 詳細 #{tenki.fetch("description")} \n 平均気温 #{main.fetch("temp")} \n 最高気温 #{main.fetch("temp_max")} \n 最低気温 #{main.fetch("temp_min")}" 
+          response = " sss #{tenki.fetch("main")} \n 詳細 #{tenki.fetch("description")} \n 平均気温 #{main.fetch("temp")} \n 最高気温 #{main.fetch("temp_max")} \n 最低気温 #{main.fetch("temp_min")}" 
         elsif status == 500
           response = "検索結果がありません"
         end
