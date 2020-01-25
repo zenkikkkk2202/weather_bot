874f77b1f87b2d37951cd2651a6878f26e2aeea2
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-25 20:31:14 +0900

    sample

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -69,7 +69,7 @@ class WeatherController < ApplicationController
         eurl = URI.encode("https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=#{gkey}&address=#{area}")
         result = `curl -s -X GET "#{eurl}"`
         eresult = JSON.parse result
-        info = eresult.fetch("rest")[0]
+        info = eresult.fetch("rest").sample.(3)
         info2 = eresult.fetch("rest")[1]
         name = info.fetch("name")
         cate = info.fetch("category")
