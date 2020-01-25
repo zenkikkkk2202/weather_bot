aaf8d742f588f63013f1e7b2e7403398179a0334
zenkikkkk2202 <57906987+zenkikkkk2202@users.noreply.github.com>
2020-01-25 20:59:44 +0900

    change

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -72,10 +72,14 @@ class WeatherController < ApplicationController
         infos = eresult.fetch("rest").sample(3)
         # info2 = eresult.fetch("rest")[1]
           infos.each do |info|
-            name = info.fetch("name")
-            cate = info.fetch("category")
-            url = info.fetch("url")
+            name = info.fetch("name")[0]
+            cate = info.fetch("category")[0]
+            url = info.fetch("url")[0]
             response = "店名 #{name} \nカテゴリー #{cate} \nURL #{url}"
+            name2 = info.fetch("name")[1]
+            cate2 = info.fetch("category")[1]
+            url2 = info.fetch("url")[1]
+            response = "店名 #{name2} \nカテゴリー #{cate2} \nURL #{url2}"
           end
         # name2 = info2.fetch("name")
         # cate2 = info2.fetch("category")
